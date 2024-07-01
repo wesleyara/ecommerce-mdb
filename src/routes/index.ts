8e1c8343c2cfd81133952ae1b86ab7aaf62fa393
wesleyara <wes1gom@gmail.com>
2024-07-01 20:35:25 -0300

    feat: Add endpoint for creating products

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { AccountController } from "../controllers/AccountController";
+import { ProductController } from "../controllers/ProductController";
 
 const router = express.Router();
 
 const accountController = new AccountController();
+const productController = new ProductController();
 
 router.post("/accounts/create", (req, res) => accountController.createAccount(req,res));
 router.post("/accounts/login", (req, res) => accountController.login(req,res));
 router.get("/accounts", (req, res) => accountController.getAccount(req,res));
+
+router.post("/products/create", (req, res) => productController.createProduct(req,res));
 
 export default router;
