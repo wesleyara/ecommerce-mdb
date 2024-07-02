1b963ea6894e0b0fbaacf1a48dc73e339d87a79a
wesleyara <wes1gom@gmail.com>
2024-07-02 01:34:10 -0300

    feat: Add RepositoryUpdateManyProducts interface for updating multiple products with a single request

--- src/types/repository.ts
+++ src/types/repository.ts
@@ -15,11 +15,22 @@ export interface RepositoryUpdateProduct {
   remove_category?: boolean;
 }
 
+export interface RepositoryUpdateManyProducts {
+  productIds: unknown[];
+  category_id: unknown;
+  remove_category?: boolean;
+}
+
 // --- Category ---
 export interface RepositoryCreateCategory {
   title: string;
   description: string;
   owner_id: unknown;
+}
+export interface RepositoryUpdateCategory {
+  categoryId: unknown;
+  title: string;
+  description: string;
 }
 
 // --- Others ---
