d300e21079fdec50173139d937c6c5ba730b3524
wesleyara <wes1gom@gmail.com>
2024-07-01 23:56:46 -0300

    feat: Update AccountRepository to remove unused import and fix updateRelations method signature

--- src/repositories/AccountRepository.ts
+++ src/repositories/AccountRepository.ts
@@ -1,5 +1,6 @@
 import { Account } from "../models/AccountModel";
-import { CreateAccountProps, UpdateRelationsProps } from "../types";
+import { CreateAccountProps } from "../types";
+import { UpdateRelationsProps } from "../types/repository";
 
 export class AccountRepository {
   async createAccount({ name, email, password }: CreateAccountProps) {
@@ -20,9 +21,9 @@ export class AccountRepository {
 
   async findAccountByEmail(email: string) {
     try {
-      const account = await Account.findOne({ email });
-      await account?.populate("products");
-      await account?.populate("categories");
+      const account = await Account.findOne({ email })
+        .populate("products")
+        .populate("categories");
 
       return account;
     } catch (error) {
@@ -32,9 +33,9 @@ export class AccountRepository {
 
   async findAccountById(id: string) {
     try {
-      const account = await Account.findById(id);
-      await account?.populate("products");
-      await account?.populate("categories");
+      const account = await Account.findById(id)
+        .populate("products")
+        .populate("categories");
 
       return account;
     } catch (error) {
@@ -42,9 +43,9 @@ export class AccountRepository {
     }
   }
 
-  async updateRelations({ accountId, type, typeId }: UpdateRelationsProps) {
+  async updateRelations({ modelId, type, typeId }: UpdateRelationsProps) {
     try {
-      await Account.findByIdAndUpdate(accountId, {
+      await Account.findByIdAndUpdate(modelId, {
         $push: { [type]: typeId },
       });
     } catch (error) {
